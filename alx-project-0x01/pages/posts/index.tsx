15d07d6c598fe3f2f100a8121bf6b3e001b880de
Aalphakeem-Adroit <alphakeem12@gmail.com>
2025-09-21 08:58:40 +0100

    Setting navigation between pages

---
+++ alx-project-0x01/pages/posts/index.tsx
@@ -0,0 +1,7 @@
+import Header from '@/components/layout/Header';
+
+const PostPage: React.FC = () => {
+    return <Header />
+}
+
+export default PostPage;
---
+++ alx-project-0x01/pages/users/index.tsx
@@ -0,0 +1,7 @@
+import Header from '@/components/layout/Header';
+
+const UserPage: React.FC = () => {
+    return <Header />
+}
+
+export default UserPage;
