cd4c0b2e428304a2bbca3633c3718d86052dd917
Aalphakeem-Adroit <alphakeem12@gmail.com>
2025-09-21 11:38:41 +0100

    Fix: post.map

--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -21,7 +21,7 @@ const Users: React.FC<{ users: UserProps[] }> = ({ users }) => {
           <button onClick={() => setModalOpen(true)} className="bg-blue-700 px-4 py-2 rounded-full text-white">Add User</button>
         </div>
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mt-6">
-          {users.map((user) => (
+          {posts.map((user) => (
             <UserCard {...user} key={user.id} />
           ))}
         </div>
